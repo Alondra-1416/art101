111ecdc1ce4f2584d3d4d5ba6c9e8c7b0e961531
Alondra-1416 <alsahern@ucsc.edu>
2024-10-22 03:27:59 -0700

    Lab 5 changes

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,4 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
 
-// Constants
-
-// Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
---
+++ lab5/js/lab5.js
@@ -0,0 +1,23 @@
+// index.js - The goal of this project is to experiment with the basics JavaScript data types and Variables 
+// Author: Alondra Hernandez 
+// Date: October 21 2024
+
+// Constants
+
+// Functions 
+
+// Variables 
+make = "Ford"; 
+model = "Bronco"; 
+color = "Blush Pink"; 
+year = 1966; 
+
+// calculate 
+age = 2024 - year; 
+
+// output 
+document.writeln("Make: " + make + "<br>"); 
+document.writeln("Model: " + model + "<br>");  
+document.writeln("Color: " + color + "<br>"); 
+document.writeln("Year: " + year + "<br>"); 
+document.writeln("Age: " + age + " years<br>");  
